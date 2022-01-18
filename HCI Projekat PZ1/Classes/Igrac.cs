93c5afbe32c14419edddb42c9dd709cd072ef985
Đorđe Božović <79928843+Djordje99@users.noreply.github.com>
2022-01-18 15:44:45 +0100

    HCI PZ1

---
+++ HCI Projekat PZ1/Classes/Igrac.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Classes
+{
+    [Serializable]
+    public class Igrac
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int BrDresa { get; set; }
+        public DateTime DatumR { get; set; }
+        public string Pozicija { get; set; }
+
+        public string Slika { get; set; }
+
+        public string Opis { get; set; }
+
+        public string SekundarnaP { get; set; }
+         
+
+        public Igrac (string ime, string prezime, int brDresa, DateTime datumR, string pozicija, string slika, string opis, string sekundarnaP)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            BrDresa = brDresa;
+            DatumR = datumR;
+            Pozicija = pozicija;
+            Slika = slika;
+            Opis = opis;
+            SekundarnaP = sekundarnaP;
+        }
+
+        public Igrac() { }
+    }
+}
---
+++ HCI Projekat PZ1/Projekat/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Classes;
+
+namespace Projekat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static public BindingList<Igrac> Igraci { get; set; } = new BindingList<Igrac>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+
+            DataContext = this;
+
+        }
+
+        private void Dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            UnosIgraca ui = new UnosIgraca();
+            ui.ShowDialog();
+        }
+
+        private void Izlaz_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Igrac ig = (Igrac)((Button)e.Source).DataContext;
+            File.Delete(ig.Opis);
+            Igraci.Remove(ig);
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            Igrac ig = (Igrac)((Button)e.Source).DataContext;
+            UnosIgraca ui = new UnosIgraca(ig);
+            ui.ShowDialog();
+        }
+
+        private void View_Click(object sender, RoutedEventArgs e)
+        {
+            Igrac ig = (Igrac)((Button)e.Source).DataContext;
+            ViewWindow vw = new ViewWindow(ig);
+            vw.ShowDialog();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
